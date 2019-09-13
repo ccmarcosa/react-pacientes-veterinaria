b9d737f2679f37950553a19af51f8f05bf81979d
ccmarcosa <ccmarcosa@gmail.com>
2019-09-13 10:20:17 -0500

    Mostrando las citas del state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './bootstrap.min.css';
 import Header from './components/Header';
 import NuevaCita from './components/NuevaCita';
+import ListaCitas from './components/ListaCitas';
 
 class App extends Component {
   state = {
@@ -11,9 +12,7 @@ class App extends Component {
   crearNuevaCita = datos => {
     // copiar el state actual
     const citas = [...this.state.citas, datos];
-  
-    console.log('HOLAAAAAA!!!');
-    
+
     // agregar al nuevo state
     this.setState({
       citas
@@ -31,6 +30,12 @@ class App extends Component {
           <div className="col-md-10 mx-auto">
             <NuevaCita 
               crearNuevaCita={this.crearNuevaCita}
+            />
+          </div>
+
+          <div className="mt-5 col-md-10 mx-auto">
+            <ListaCitas 
+              citas={this.state.citas}
             />
           </div>
         </div>
--- src/components/NuevaCita.js
+++ src/components/NuevaCita.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import uuid from 'uuid';
 
-class NuevaCita extends Component {
-	state = {
-		cita: {
+const stateInicial = {
+	cita: {
 			mascota: '',
 			propietario: '',
 			fecha: '',
@@ -11,7 +10,10 @@ class NuevaCita extends Component {
 			sintomas: ''
 		},
 		error: false
-	}
+}
+
+class NuevaCita extends Component {
+	state = { ...stateInicial }
 	
 	// cuando el usuario escribe en los inputs
 	handleChange = e => {
@@ -47,15 +49,27 @@ class NuevaCita extends Component {
 
 		// agregar la cita al state de App
 		this.props.crearNuevaCita(nuevaCita);
+
+		// colocar en el state el stateInicial
+		this.setState({
+			...stateInicial
+		});
 	}
 
 	render(){
+		
+		// extraer valor del state
+		const { error } = this.state;
+
 		return(
 			<div className="card mt-5 py-5">
 				<div className="card-body">
 					<h2 className="card-title text-center mb-5">
 						Llena el formulario para crear una nueva cita
 					</h2>
+
+					{ error ? <div className="alert alert-danger mt-2 mb-5 text-center">
+					Todos los campos son obligatorios</div>: null }
 
 					<form onSubmit={this.handleSubmit}>
 						<div className="form-group row">
